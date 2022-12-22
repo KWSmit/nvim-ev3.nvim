bad6731464be31223b07a32dc341ae5dd0990680
Kees Smit <kees@kwsmit.nl>
2022-12-22 15:03:17 +0100

    221222 - Add configuration for user
    Add setup function in ev3-nvim.lua. Now the user can setup the projects
    directory to his own needs.

--- lua/nvim-ev3.lua
+++ lua/nvim-ev3.lua
@@ -3,18 +3,22 @@ local pt = require "nvim-ev3.project_tools"
 
 local M = {}
 
-local base_projects_dir = "/home/kees/Sources/nvim-plugins/test/"
+local _config = {}
+
 local project_name
 local user = ""
 local host = ""
 local interpreter = ""
 local project_loaded = false
 
+function M.setup(config)
+    _config = config
+end
 
 function M.create_ev3_project()
     -- Create a new project for EV3
-    -- Change pwd for nvim to base_projects_dir
-    vim.cmd('cd ' .. base_projects_dir)
+    -- Change pwd for nvim to projects directory
+    vim.cmd('cd ' .. _config.projects_dir)
     -- Ask user for project name
     vim.ui.input({
         prompt = "Enter project name: ",
@@ -80,20 +84,19 @@ function M.create_ev3_project()
     -- If it's a new project: create project directory
     if new_project then
         os.execute('mkdir ' .. project_name)
-        print('mkdir ' .. project_name)
     end
     -- Write project data for new project or overwrite existing project
     -- TODO clear all files in case of existing project
     if new_project or overwrite then
         -- Write project file (.project.ini)
-        project_file = base_projects_dir .. project_name .. '/.project.ini'
+        project_file = _config.projects_dir .. project_name .. '/.project.ini'
         pt.write_project_file(project_file, project_name,
                               username, host, interpreter)
         -- Write empty main file (main.py)
         if (interpreter == 'python') then
-            pt.write_main_python(base_projects_dir, project_name)
+            pt.write_main_python(_config.projects_dir, project_name)
         else
-            pt.write_main_micro_python(base_projects_dir, project_name)
+            pt.write_main_micro_python(_config.projects_dir, project_name)
         end
     end
     -- Done, project is loaded
@@ -102,12 +105,11 @@ end
 
 function M.open_ev3_project()
     -- Open an existing EV3 project
-    -- Set pwd to base_projects_dir
-    vim.cmd('cd' .. base_projects_dir)
-    -- Get all project directories in base_projects_dir
+    -- Set pwd to projects directory
+    vim.cmd('cd ' .. _config.projects_dir)
+    -- Get all project directories in projects directory
     dir_list = {}
-    for dir in io.popen('ls -d ' .. base_projects_dir .. '*'):lines() do
-        -- print(dir)
+    for dir in io.popen('ls -d ' .. _config.projects_dir .. '*'):lines() do
         table.insert(dir_list, vim.fn.fnamemodify(dir, ':t'))
     end
     -- Let user select desired project
@@ -119,7 +121,7 @@ function M.open_ev3_project()
     }, function(dir, idx)
         if dir then
             project_name = dir
-            project_file = base_projects_dir .. project_name .. "/.project.ini"
+            project_file = _config.projects_dir .. project_name .. "/.project.ini"
             user, host, interpreter = pt.read_project_file(project_file)
             print("  -  Project " .. project_name .. " successfully opened")
             project_loaded = true
@@ -165,11 +167,13 @@ function M.check_battery()
     if (project_loaded == false) then
         print("First open an EV3-project!")
     else
-       local handle = io.popen('scp -l 2000 robot@192.168.2.14:/sys/class/power_supply/lego-ev3-battery/voltage_now /dev/stdout')
-       local result = handle:read(7)
-       result = tonumber(result) / 10e5
-       print("Current battery voltage: " .. result .. "volt")
-       handle.close()
+        local bat_file = ':/sys/class/power_supply/lego-ev3-battery/voltage_now'
+        local handle = io.popen('scp -l 2000 ' .. user .. '@' .. host ..
+                                bat_file .. ' /dev/stdout 2> /dev/null')
+        local result = handle:read()
+        result = tonumber(result) / 10e5
+        print("Current battery voltage: " .. result .. " v")
+        handle.close()
     end
 end
 
