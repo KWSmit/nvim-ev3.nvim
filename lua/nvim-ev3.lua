8acbadae8ac959dcd280ec28f57ca834f3c1cef1
Kees Smit <kees@kwsmit.nl>
2022-12-27 13:58:16 +0100

    221227 Refactoring nvim-ev3.lua
    Refactor nvim-ev3.lua, move code over to nvim-ev3/project_tools.
    Better handling for the creation of a new project,
    for the case the user doesn't enter a project name.

--- lua/nvim-ev3.lua
+++ lua/nvim-ev3.lua
@@ -20,88 +20,51 @@ function M.create_ev3_project()
     -- Change pwd for nvim to projects directory
     vim.cmd("cd " .. _config.projects_dir)
     -- Ask user for project name
-    vim.ui.input({
-        prompt = "Enter project name: ",
-    }, function(input)
-        if input then
-            project_name = input
+    project_name = pt.ask_project_name()
+    if project_name then
+        -- Ask user for username
+        user = pt.ask_username()
+        -- Ask user for hostname (or ip-address)
+        host = pt.ask_hostname()
+        -- Ask user for interpreter (python or micro-python)
+        interpreter = pt.ask_interpreter()
+        -- Check if it's a new or an existing project
+        if utils.isdir(project_name) then
+            -- Existing project
+            new_project = false
+            -- Ask user to overwrite project or not
+            vim.ui.input({
+                prompt = "Project already exists! Overwrite? [y,N]: "
+            }, function(input)
+                if input == "y" then
+                    overwrite = true
+                    -- Delete all existing project files
+                    vim.cmd("!rm -rf " .. project_name .. "/*")
+                else
+                    overwrite = false
+                end
+            end)
         else
-            print("You cancelled")
+            -- Directory does not exist, so it's a new project
+            new_project = true
+            -- Create project directory
+            os.execute("mkdir " .. project_name)
         end
-    end)
-    -- Ask user for username
-    vim.ui.input({
-        prompt = "Enter username: ",
-        default = "robot",
-    }, function(input)
-        if input then
-            username = input
-        else
-            print("You cancelled")
+        -- Write project data for new project or overwrite existing project
+        if new_project or overwrite then
+            -- Write project file (.project.ini)
+            project_file = _config.projects_dir .. project_name .. "/.project.ini"
+            pt.write_project_file(project_file, project_name,
+                                  user, host, interpreter)
+            -- Write empty main file (main.py)
+            pt.write_main_file(interpreter, _config.projects_dir, project_name)
         end
-    end)
-    -- Ask user for hostname (or ip-address)
-    vim.ui.input({
-        prompt = "Enter host: ",
-    }, function(input)
-        if input then
-            host = input
-        else
-            print("You cancelled")
-        end
-    end)
-    -- Ask user for interpreter (python or micro-python)
-    vim.ui.input({
-        prompt = "Choose interpreter: 1=python, 2=micro-python: ",
-        default = 2
-    }, function(input)
-        if input == "1" then
-            interpreter = "python"
-        elseif input == "2" then
-            interpreter = "micro-python"
-        else
-            interpreter = "micro-python"
-        end
-    end)
-    -- Check if it's a new or an existing project
-    if utils.isdir(project_name) then
-        -- Existing project
-        new_project = false
-        -- Ask user to overwrite project or not
-        vim.ui.input({
-            prompt = "Project already exists! Overwrite? [y,N]: "
-        }, function(input)
-            if input == "y" then
-                overwrite = true
-                -- Delete all existing project files
-                vim.cmd("!rm -rf " .. project_name .. "/*")
-            else
-                overwrite = false
-            end
-        end)
+        -- Done, project is loaded
+        project_loaded = true
     else
-        -- Directory does not exist, so it's a new project
-        new_project = true
+        -- User didn't enter a project name, so project cannot be created
+        print("You cancelled the creation of a new project")
     end
-    -- If it's a new project: create project directory
-    if new_project then
-        os.execute("mkdir " .. project_name)
-    end
-    -- Write project data for new project or overwrite existing project
-    if new_project or overwrite then
-        -- Write project file (.project.ini)
-        project_file = _config.projects_dir .. project_name .. "/.project.ini"
-        pt.write_project_file(project_file, project_name,
-                              username, host, interpreter)
-        -- Write empty main file (main.py)
-        if (interpreter == "python") then
-            pt.write_main_python(_config.projects_dir, project_name)
-        else
-            pt.write_main_micro_python(_config.projects_dir, project_name)
-        end
-    end
-    -- Done, project is loaded
-    project_loaded = true
 end
 
 function M.open_ev3_project()
--- lua/nvim-ev3/project_tools.lua
+++ lua/nvim-ev3/project_tools.lua
@@ -3,11 +3,77 @@ local utils = require "nvim-ev3.utils"
 
 local M = {}
 
+function M.ask_project_name()
+    -- Ask user for and return project Name
+    result = nill
+    vim.ui.input({
+        prompt = "Enter project name: ",
+    }, function(input)
+        if input then
+            -- User did enter a name
+            result = input
+        else
+            -- User did not enter anything, just pressed enter 
+            print("You need to enter a project name to create a new project.")
+        end
+    end)
+    return result
+end
+
+function M.ask_username()
+    -- Ask and return username
+    result = nill
+    vim.ui.input({
+        prompt = "Enter username: ",
+        default = "robot",
+    }, function(input)
+        if input then
+            result = input
+        end
+    end)
+    return result
+end
+
+function M.ask_hostname()
+    -- Ask and return hostname
+    result = nill
+    vim.ui.input({
+        prompt = "Enter host: ",
+    }, function(input)
+        if input then
+            result = input
+        end
+    end)
+    return result
+end
+
+function M.ask_interpreter()
+    -- Ask and return interpreter
+    result = nill
+    vim.ui.input({
+        prompt = "Choose interpreter: 1=python, 2=micro-python: ",
+        default = 2
+    }, function(input)
+        if input == "1" then
+            result = "python"
+        else
+            result = "micro-python"
+        end
+    end)
+    return result
+end
+
 function M.write_project_file(project_file, project_name, 
                               user, host, interpreter)
     -- Write data to project file
     f = io.open(project_file, "w")
     io.output(f)
+    if not user then
+        user = ""
+    end
+    if not host then
+        host = ""
+    end
     io.write("USER=" .. user .. "\n")
     io.write("HOST=" .. host .. "\n")
     io.write("DIR=home/" .. user .. "/" .. project_name .. "\n")
@@ -37,64 +103,63 @@ function M.read_project_file(project_file)
     return user, host, interpreter
 end
 
-function M.write_main_python(base_projects_dir, project_name)
-    -- Write main.py for python interpreter
-    python_code = 
-    "#!/user/bin/env python3 \n" ..
-    "# \n" ..
-    "# Name:        main.py \n" ..
-    "# Description:  \n" ..
-    "# Author:       \n" ..
-    "# Version:      \n" ..
-    "# Date:         \n" ..
-    "# \n" ..
-    "from ev3dev2.motor import (LargeMotor, MediumMotor,\n" ..
-    "                           MoveTank, MoveSteering, MoveJoystick,\n" ..
-    "                           OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D)\n" ..
-    "from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4\n" ..
-    "from ev3dev2.sensor.lego import (TouchSensor, ColorSensor,\n" ..
-    "                                 UltrasonicSensor, InfraredSensor)\n" ..
-    "from ev3dev2.led import Leds\n" ..
-    "from ev3dev2.button import Button\n" ..
-    "from ev3dev2.sound import Sound\n" ..
-    "from ev3dev2.display import Display\n" ..
-    "\n"
-    main_file = base_projects_dir .. project_name .. "/main.py"
-    f = io.open(main_file, "w")
-    f:write(python_code)
-    io.close(f)
-end
-
-function M.write_main_micro_python(base_projects_dir, project_name)
-    -- Write main.py for micro-python interpreter
-    micro_python_code = 
-    "#!/usr/bin/env pybricks-micropython \n" ..
-    "# \n" ..
-    "# Name:        main.py \n" ..
-    "# Description:  \n" ..
-    "# Author:       \n" ..
-    "# Version:      \n" ..
-    "# Date:         \n" ..
-    "# \n" ..
-    "from pybricks.hubs import EV3Brick \n" ..
-    "from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, \n" ..
-    "                                 InfraredSensor, UltrasonicSensor, GyroSensor) \n" ..
-    "from pybricks.parameters import Port, Stop, Direction, Button, Color \n" ..
-    "from pybricks.tools import wait, StopWatch, DataLog \n" ..
-    "from pybricks.robotics import DriveBase \n" ..
-    "from pybricks.media.ev3dev import SoundFile, ImageFile \n" ..
-    "\n" ..
-    "\n" ..
-    "# Create your objects here. \n" ..
-    "ev3 = EV3Brick()\n " ..
-    "\n" ..
-    "\n" ..
-    "# Write your program here. \n" ..
-    "ev3.speaker.beep()\n"
-    main_file = base_projects_dir .. project_name .. "/main.py"
-    f = io.open(main_file, "w")
-    f:write(micro_python_code)
-    io.close(f)
+function M.write_main_file(interpreter, base_projects_dir, project_name)
+    -- Write new main.py
+    if interpreter == "python" then
+        python_code = 
+        "#!/user/bin/env python3 \n" ..
+        "# \n" ..
+        "# Name:        main.py \n" ..
+        "# Description:  \n" ..
+        "# Author:       \n" ..
+        "# Version:      \n" ..
+        "# Date:         \n" ..
+        "# \n" ..
+        "from ev3dev2.motor import (LargeMotor, MediumMotor,\n" ..
+        "                           MoveTank, MoveSteering, MoveJoystick,\n" ..
+        "                           OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D)\n" ..
+        "from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4\n" ..
+        "from ev3dev2.sensor.lego import (TouchSensor, ColorSensor,\n" ..
+        "                                 UltrasonicSensor, InfraredSensor)\n" ..
+        "from ev3dev2.led import Leds\n" ..
+        "from ev3dev2.button import Button\n" ..
+        "from ev3dev2.sound import Sound\n" ..
+        "from ev3dev2.display import Display\n" ..
+        "\n"
+        main_file = base_projects_dir .. project_name .. "/main.py"
+        f = io.open(main_file, "w")
+        f:write(python_code)
+        io.close(f)
+    else
+        micro_python_code = 
+        "#!/usr/bin/env pybricks-micropython \n" ..
+        "# \n" ..
+        "# Name:        main.py \n" ..
+        "# Description:  \n" ..
+        "# Author:       \n" ..
+        "# Version:      \n" ..
+        "# Date:         \n" ..
+        "# \n" ..
+        "from pybricks.hubs import EV3Brick \n" ..
+        "from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, \n" ..
+        "                                 InfraredSensor, UltrasonicSensor, GyroSensor) \n" ..
+        "from pybricks.parameters import Port, Stop, Direction, Button, Color \n" ..
+        "from pybricks.tools import wait, StopWatch, DataLog \n" ..
+        "from pybricks.robotics import DriveBase \n" ..
+        "from pybricks.media.ev3dev import SoundFile, ImageFile \n" ..
+        "\n" ..
+        "\n" ..
+        "# Create your objects here. \n" ..
+        "ev3 = EV3Brick()\n " ..
+        "\n" ..
+        "\n" ..
+        "# Write your program here. \n" ..
+        "ev3.speaker.beep()\n"
+        main_file = base_projects_dir .. project_name .. "/main.py"
+        f = io.open(main_file, "w")
+        f:write(micro_python_code)
+        io.close(f)
+    end
 end
 
 return M
